c8709cd777bb40ffe84a237d0a707f3088ed462f
Charan <shhrikatta@gmail.com>
2015-09-24 16:56:15 +0530

    changed default boolean to f instead of 0

--- db/migrate/20101116224140_create_api_keys.rb
+++ db/migrate/20101116224140_create_api_keys.rb
@@ -5,8 +5,8 @@ class CreateApiKeys < ActiveRecord::Migration
       t.integer :device_id
       t.integer :feed_id
       t.integer :user_id
-      t.boolean :write_flag, :default => 0
-      t.boolean :public_flag, :default => 0
+      t.boolean :write_flag, :default => 'f'
+      t.boolean :public_flag, :default => 'f'
 
       t.timestamps
     end
